3d7d3eb00122b0ac9a30bd650d2e9d3b1de309c3
Oriol Gual <oriol.gual@gmail.com>
2014-11-06 10:20:50 +0100

    Extract methods

--- lib/srt_processor/processor.rb
+++ lib/srt_processor/processor.rb
@@ -6,17 +6,9 @@ module SrtProcessor
     end
 
     def shift(seconds)
-      output_dialogs = input_subtitle.dialogs.map do |dialog|
-        start_time = shift_time(dialog.start_time, seconds)
-        end_time = shift_time(dialog.end_time, seconds)
-        Dialog.new(dialog.id, start_time, end_time, dialog.text)
-      end
-
-      contents = output_dialogs.map do |dialog|
-        [dialog.id, [dialog.start_time, dialog.end_time].join(' --> '), dialog.text].join("\r\n")
-      end.join("\r\n\r\n")
-
-      IO.write(@output, contents)
+      dialogs = output_dialogs(seconds)
+      content = output_content(dialogs)
+      export(content)
     end
 
     def input_subtitle
@@ -24,6 +16,23 @@ module SrtProcessor
     end
 
     private
+    def export(content)
+      IO.write(@output, content)
+    end
+
+    def output_content(dialogs)
+      dialogs.map do |dialog|
+        [dialog.id, [dialog.start_time, dialog.end_time].join(' --> '), dialog.text].join("\r\n")
+      end.join("\r\n\r\n")
+    end
+
+    def output_dialogs(seconds)
+      input_subtitle.dialogs.map do |dialog|
+        start_time = shift_time(dialog.start_time, seconds)
+        end_time = shift_time(dialog.end_time, seconds)
+        Dialog.new(dialog.id, start_time, end_time, dialog.text)
+      end
+    end
 
     def shift_time(time, shift)
       hours, minutes, seconds_and_mili = time.split(':')
