9a29aafa267a341aa031933763b19f13c6984b2a
Oriol Gual <oriol.gual@gmail.com>
2014-11-06 08:33:43 +0100

    Unrefactor

--- lib/srt_processor/processor.rb
+++ lib/srt_processor/processor.rb
@@ -6,12 +6,30 @@ module SrtProcessor
     end
 
     def shift(seconds)
-      output_dialogs = Shifter.new(input_subtitle).shift(seconds)
-      Exporter.new(@output, output_dialogs).export
+      output_dialogs = input_subtitle.dialogs.map do |dialog|
+        start_time = shift_time(dialog.start_time, seconds)
+        end_time = shift_time(dialog.end_time, seconds)
+        Dialog.new(dialog.id, start_time, end_time, dialog.text)
+      end
+
+      contents = output_dialogs.map do |dialog|
+        [dialog.id, [dialog.start_time, dialog.end_time].join(' --> '), dialog.text].join("\r\n")
+      end.join("\r\n\r\n")
+
+      IO.write(@output, contents)
     end
 
     def input_subtitle
       @input_subtitle ||= Subtitle.new(@input)
+    end
+
+    private
+
+    def shift_time(time, shift)
+      hours, minutes, seconds_and_mili = time.split(':')
+      seconds, mili = seconds_and_mili.split(',')
+      seconds_and_mili = [seconds.to_i + shift, mili].join(',')
+      [hours, minutes, seconds_and_mili].join(':')
     end
   end
 end
