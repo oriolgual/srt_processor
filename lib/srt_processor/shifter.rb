bc96a79ecd36c35094d7e7ed35b48c3fdfbf8c2c
Oriol Gual <oriol.gual@gmail.com>
2014-11-06 14:13:59 +0100

    Shifting can be ugly as hell

--- lib/srt_processor/shifter.rb
+++ lib/srt_processor/shifter.rb
@@ -6,9 +6,36 @@ module SrtProcessor
 
     def shift(time_to_shift)
       hours, minutes, seconds_and_mili = @timestamp.split(':')
-      seconds, mili = seconds_and_mili.split(',')
-      seconds_and_mili = [seconds.to_i + time_to_shift, mili].join(',')
-      [hours, minutes, seconds_and_mili].join(':')
+      seconds, milliseconds = seconds_and_mili.split(',')
+
+      if time_to_shift < 1
+        milliseconds = (milliseconds.to_i + time_to_shift * 1000).to_i
+
+        if milliseconds > 999
+          seconds_to_add = (milliseconds.to_f / 1000).floor.to_i
+          seconds = seconds.to_i + seconds_to_add
+          milliseconds -= seconds_to_add * 1000
+        end
+      end
+
+      if time_to_shift >= 1
+        seconds = seconds.to_i + time_to_shift
+
+        if seconds > 59
+          minutes_to_add = (seconds.to_f / 60).floor.to_i
+          minutes = minutes.to_i + minutes_to_add
+          seconds -= minutes_to_add * 60
+        end
+
+        if minutes.to_i > 59
+          hours_to_add = (minutes.to_f / 60).floor.to_i
+          hours = hours.to_i + hours_to_add
+          minutes -= hours_to_add * 60
+        end
+      end
+
+      [hours.to_s.rjust(2, '0'), minutes.to_s.rjust(2, '0'), [seconds.to_s.rjust(2, '0'), milliseconds].join(',')].join(':')
     end
+
   end
 end
--- spec/lib/shifter_spec.rb
+++ spec/lib/shifter_spec.rb
@@ -3,12 +3,49 @@ require 'spec_helper'
 module SrtProcessor
   describe Shifter do
     describe 'shift' do
-      it 'shifts a subtitle timestamp' do
-        timestamp = "00:02:26,371"
+      let(:timestamp) { "00:02:26,371" }
+      let(:shifter)   { Shifter.new(timestamp) }
 
-        shifted_timestamp = Shifter.new(timestamp).shift(2)
+      it 'shifts a subtitle timestamp' do
+        shifted_timestamp = shifter.shift(2)
 
         expect(shifted_timestamp).to eq("00:02:28,371")
+      end
+
+      context 'when the shifting time is less than one second' do
+        it 'shifts the timestamp in milliseconds' do
+          shifted_timestamp = shifter.shift(0.029)
+
+          expect(shifted_timestamp).to eq("00:02:26,400")
+        end
+      end
+
+      context 'when shifting changes the minute' do
+        it 'shifts the timestamp correctly' do
+          shifted_timestamp = shifter.shift(40)
+
+          expect(shifted_timestamp).to eq("00:03:06,371")
+        end
+      end
+
+      context 'when shifting changes the hour' do
+        let(:timestamp) { "00:59:59,000" }
+
+        it 'shifts the timestamp correctly' do
+          shifted_timestamp = shifter.shift(2)
+
+          expect(shifted_timestamp).to eq("01:00:01,000")
+        end
+      end
+
+      context 'when shifting changes the seconds' do
+        let(:timestamp) { "00:00:00,900" }
+
+        it 'shifts the timestamp correctly' do
+          shifted_timestamp = shifter.shift(0.5)
+
+          expect(shifted_timestamp).to eq("00:00:01,400")
+        end
       end
     end
   end
