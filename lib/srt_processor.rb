9a23b6797c4af44a410785ec980c981633b3aa4e
Oriol Gual <oriol.gual@gmail.com>
2014-11-06 09:54:33 +0100

    Remove code

--- lib/srt_processor.rb
+++ lib/srt_processor.rb
@@ -1,8 +1,6 @@
 require "srt_processor/version"
 require "srt_processor/subtitle"
-require "srt_processor/shifter"
 require "srt_processor/processor"
-require "srt_processor/exporter"
 require "srt_processor/dialog"
 
 module SrtProcessor
